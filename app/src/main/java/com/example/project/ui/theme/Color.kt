ad132549b6f29e072f3522b2db0804bbaa1696f8
Azim <70862714+Explos1ve@users.noreply.github.com>
2023-05-12 17:05:49 +0500

    project added

---
+++ app/src/main/java/com/example/project/ui/theme/Color.kt
@@ -0,0 +1,8 @@
+package com.example.project.ui.theme
+
+import androidx.compose.ui.graphics.Color
+
+val Teal200 = Color(0xFF03DAC5)
+val MediumGray = Color(0xFF2E2E2E)
+val LightGray = Color(0xFF818181)
+val Blue = Color(0XFF879FFF)
